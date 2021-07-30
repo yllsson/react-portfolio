2acf37645292c5126fb671183dc85ed85de32868
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-30 21:08:08 +0100

    Amending contact tile icon names due to base FA classes differing

--- src/components/ContactTile.js
+++ src/components/ContactTile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactTile = ({ text, url, iconName }) => {
   return (
     <a className='contact-link' href={url} target='_blank'>
-      <i className={`fab ${iconName}`}></i>
+      <i className={`${iconName}`}></i>
       <span>{text}</span>
     </a>
   );
--- src/data/contactData.js
+++ src/data/contactData.js
@@ -2,22 +2,22 @@ const contactData = [
   {
     text: '@yllsson',
     url: 'https://twitter.com/yllsson',
-    iconName: 'fa-twitter'
+    iconName: 'fab fa-twitter'
   },
   {
     text: 'yllsson',
     url: 'https://github.com/yllsson',
-    iconName: 'fa-github'
+    iconName: 'fab fa-github'
   },
   {
     text: 'ylvaeturner@gmail.com',
     url: 'mailto:ylvaeturner@gmail.com',
-    iconName: 'fa-envelope'
+    iconName: 'fas fa-envelope'
   },
   {
     text: 'Ylva Turner',
     url: 'https://www.linkedin.com/in/ylva-turner/',
-    iconName: 'fa-linkedin'
+    iconName: 'fab fa-linkedin'
   }
 ];
 
